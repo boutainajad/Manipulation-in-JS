5d7ef23ded4198ebd72464c38a2fb9bf476084e7
Your Name <you@example.com>
2025-12-07 15:28:39 +0100

    Exercice 3: Générer un tableau cumulatif

--- ex.js
+++ ex.js
@@ -1,59 +1,84 @@
 const data = [12, 5, 8, 130, 44, 9, 2, 77, 21, 56, 3, 99, 14, 28, 1, 65, 7, 38, 11, 50];
 
-// 1. Calculer le produit de tous les nombres :
+// // 1. Calculer le produit de tous les nombres :
 
-// A
+// // A
 
-const number = data.reduce((acc , num) => {
-    return acc * num ;
-},1)
-console.log(number);
+// const number = data.reduce((acc , num) => {
+//     return acc * num ;
+// },1)
+// console.log(number);
 
-// B
+// // B
 
-let mul = 1;
-for (let i = 0; i < data.length; i++){
-    mul *= data[i];
-}
-console.log(mul);
+// let mul = 1;
+// for (let i = 0; i < data.length; i++){
+//     mul *= data[i];
+// }
+// console.log(mul);
 
-//  2.Trouver la médiane
-// A
-function mediane(data){
-const sorted = [... data].sort((a, b) => a - b);
-const n = sorted.length
+// //  2.Trouver la médiane
+// // A
+// function mediane(data){
+// const sorted = [... data].sort((a, b) => a - b);
+// const n = sorted.length
 
-if (n % 2 === 1){
-    return(sorted[Math.floor(n / 2)]);
-}
-const med1 = sorted[n /2 - 1];
-const med2 = sorted[n / 2];
-return ((med1 + med2)/ 2);
-}
-console.log(mediane(data));
+// if (n % 2 === 1){
+//     return(sorted[Math.floor(n / 2)]);
+// }
+// const med1 = sorted[n /2 - 1];
+// const med2 = sorted[n / 2];
+// return ((med1 + med2)/ 2);
+// }
+// console.log(mediane(data));
 
-// B 
-function mediane2(data){
-    let sorted =[];
-    for (let i = 0; i < data.length; i++)
-    {
-        sorted[i] = data[i];
+// // B 
+// function mediane2(data){
+//     let sorted =[];
+//     for (let i = 0; i < data.length; i++)
+//     {
+//         sorted[i] = data[i];
+//     }
+//     for (let i = 0; i < sorted.length; i++)
+//     {
+//         for (let j = 0; j < sorted.length - 1; j++){
+//             if (sorted[j] > sorted[j+1]){ 
+//             let temp = sorted[j];
+//             sorted[j] = sorted[j + 1];
+//             sorted[j + 1] = temp;
+//             }
+//         }
+//     }
+//      const n = sorted.length;
+//      if (n % 2 ===1){
+//         return sorted[((n - 1) / 2)];
+//      }
+//      else 
+//         return (sorted[n / 2 - 1] + sorted [n / 2]) / 2;
+//     }
+//     console.log(mediane2(data));
+
+// 3. Générer un tableau cumulatif :
+//  A
+    function cumulatif(data){
+        let some = 0;
+        return data.reduce((result, valeur) =>{
+            some += valeur;
+            result.push(some);
+            return result;
+        }, []);
     }
-    for (let i = 0; i < sorted.length; i++)
-    {
-        for (let j = 0; j < sorted.length - 1; j++){
-            if (sorted[j] > sorted[j+1]){ 
-            let temp = sorted[j];
-            sorted[j] = sorted[j + 1];
-            sorted[j + 1] = temp;
-            }
+    console.log(cumulatif(data));
+
+//  B
+    function cumulatif2(data){
+        let some = 0;
+        let result = [];
+        for (let i = 0; i < data.length; i++){
+            some += data[i];
+            result[result.length] = some;
         }
+        return result;
+
     }
-     const n = sorted.length;
-     if (n % 2 ===1){
-        return sorted[((n - 1) / 2)];
-     }
-     else 
-        return (sorted[n / 2 - 1] + sorted [n / 2]) / 2;
-    }
-    console.log(mediane2(data));
+    console.log(cumulatif2(data));
