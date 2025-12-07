024890463e3ea6e32a9b286c0948627573e24a66
Your Name <you@example.com>
2025-12-07 19:54:13 +0100

    Exercice 7:Trouver les 3 plus petits nombres

--- ex.js
+++ ex.js
@@ -159,18 +159,54 @@ const data = [12, 5, 8, 130, 44, 9, 2, 77, 21, 56, 3, 99, 14, 28, 1, 65, 7, 38, 
 // }
 // console.log(regrouper2(data));
 
-// 6. Trouver les 3 plus grands nombres
+// // 6. Trouver les 3 plus grands nombres
 
+// // A 
+// function top3v1(data) {
+//   const sorted = [...data].sort((a, b) => b - a);
+  
+//   return sorted.slice(0, 3);
+// }
+// console.log(top3v1(data))
+
+// // B 
+// function top3v2(data) {
+//   let copie = [];
+//   for (let i = 0; i < data.length; i++) {
+//     copie[i] = data[i];
+//   }
+  
+//   for (let i = 0; i < copie.length; i++) {
+//     for (let j = 0; j < copie.length - 1; j++) {
+//       if (copie[j] < copie[j + 1]) {  
+//         let temp = copie[j];
+//         copie[j] = copie[j + 1];
+//         copie[j + 1] = temp;
+//       }
+//     }
+//   }
+  
+//   let top3 = [];
+//   for (let i = 0; i < 3; i++) {
+//     top3[i] = copie[i];
+//   }
+  
+//   return top3;
+// }
+// console.log(top3v2(data))
+
+
+// 7. Trouver les 3 plus petits nombres
 // A 
-function top3v1(data) {
-  const sorted = [...data].sort((a, b) => b - a);
+function bottom3v1(data) {
+  const sorted = [...data].sort((a, b) => a - b);
   
   return sorted.slice(0, 3);
 }
-console.log(top3v1(data))
+console.log(bottom3v2(data))
 
 // B 
-function top3v2(data) {
+function bottom3v2(data) {
   let copie = [];
   for (let i = 0; i < data.length; i++) {
     copie[i] = data[i];
@@ -178,7 +214,8 @@ function top3v2(data) {
   
   for (let i = 0; i < copie.length; i++) {
     for (let j = 0; j < copie.length - 1; j++) {
-      if (copie[j] < copie[j + 1]) {  
+      if (copie[j] > copie[j + 1]) {  
+
         let temp = copie[j];
         copie[j] = copie[j + 1];
         copie[j + 1] = temp;
@@ -186,11 +223,11 @@ function top3v2(data) {
     }
   }
   
-  let top3 = [];
+  let bottom3 = [];
   for (let i = 0; i < 3; i++) {
-    top3[i] = copie[i];
+    bottom3[i] = copie[i];
   }
   
-  return top3;
+  return bottom3;
 }
-console.log(top3v2(data))
+console.log(bottom3v2(data))
