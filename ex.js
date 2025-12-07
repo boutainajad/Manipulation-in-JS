6c109df5d86c0ebb4c418d9348b4b6f037c2efef
Your Name <you@example.com>
2025-12-07 13:30:55 +0100

    Exercice 2: Trouver la médiane

--- ex.js
+++ ex.js
@@ -16,3 +16,44 @@ for (let i = 0; i < data.length; i++){
     mul *= data[i];
 }
 console.log(mul);
+
+//  2.Trouver la médiane
+// A
+function mediane(data){
+const sorted = [... data].sort((a, b) => a - b);
+const n = sorted.length
+
+if (n % 2 === 1){
+    return(sorted[Math.floor(n / 2)]);
+}
+const med1 = sorted[n /2 - 1];
+const med2 = sorted[n / 2];
+return ((med1 + med2)/ 2);
+}
+console.log(mediane(data));
+
+// B 
+function mediane2(data){
+    let sorted =[];
+    for (let i = 0; i < data.length; i++)
+    {
+        sorted[i] = data[i];
+    }
+    for (let i = 0; i < sorted.length; i++)
+    {
+        for (let j = 0; j < sorted.length - 1; j++){
+            if (sorted[j] > sorted[j+1]){ 
+            let temp = sorted[j];
+            sorted[j] = sorted[j + 1];
+            sorted[j + 1] = temp;
+            }
+        }
+    }
+     const n = sorted.length;
+     if (n % 2 ===1){
+        return sorted[((n - 1) / 2)];
+     }
+     else 
+        return (sorted[n / 2 - 1] + sorted [n / 2]) / 2;
+    }
+    console.log(mediane2(data));
